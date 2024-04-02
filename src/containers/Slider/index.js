6495e9cf9052dc7074b86c546d86b81fc7d4e03d
TodescoAmandine <todescoamandine@yahoo.fr>
2024-04-02 18:21:03 +0200

    Filtre catégorie + slide

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
 
+
 import "./style.scss";
 
 const Slider = () => {
@@ -10,50 +11,62 @@ const Slider = () => {
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
-  const nextCard = () => {
-    setTimeout(
-      () => setIndex(index + 1 < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
-  };
-  useEffect(() => {
-    nextCard();
-  });
-  console.log(byDateDesc?.length);
+const nextCard = () => {
+  setIndex((prevIndex) =>
+    prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0
+  );
+
+  // NextCard : utilise la fonction setIndex pour mettre à jour l'index.
+  // L'index est incrémenté de 1 s'il est inférieur à la longueur du tableau trié,
+  // sinon il est réinitialisé à 0.
+};
+useEffect(
+  () => {
+    const intervalId = setInterval(nextCard, 5000);
+
+    return () => clearInterval(intervalId);
+  },
+  // eslint-disable-next-line
+  [index, byDateDesc]
+  // useEffect : exécute la fonction nextCard toutes les 5 secondes
+  // clearInterval : arrête l'exécution de la fonction nextCard
+  // La dépendance [index, byDateDesc] indique que l'effet doit être réexécuté lorsque l'une de ces valeurs change.
+);
+
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
-            </div>
+  {byDateDesc?.map((event, idx) => (
+    <div key={event.title}>
+      <div
+        className={`SlideCard SlideCard--${
+          index === idx ? "display" : "hide"
+        }`}
+      >
+        <img src={event.cover} alt="forum" />
+        <div className="SlideCard__descriptionContainer">
+          <div className="SlideCard__description">
+            <h3>{event.title}</h3>
+            <p>{event.description}</p>
+            <div>{getMonth(new Date(event.date))}</div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
-      ))}
+        </div>
+      </div>
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc.map((element, radioIdx) => (
+            <input
+              key={element.title}
+              type="radio"
+              readOnly
+              name="radio-button"
+              checked={index === radioIdx}
+            />
+          ))}
+        </div>
+      </div>
     </div>
+  ))}
+</div>
   );
 };
 
